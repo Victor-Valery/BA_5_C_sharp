ceac6ae40941ccdefd960c7bdb2dc4ec45aed495
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-04 16:34:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BA_C_SHARP_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Switch Case
+            
+            Console.WriteLine("Trafik lambasi hangi renk yaniyor: ");
+            string renk = Console.ReadLine();
+
+
+
+
+        }
+    }
+}
