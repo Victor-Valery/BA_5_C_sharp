606d00121b947d914aec3a994c1391066248a4db
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-04 16:42:23 +0200

    C_#_studies

--- Program.cs
+++ Program.cs
@@ -9,8 +9,30 @@ namespace BA_C_SHARP_5
 
             // Switch Case
             
-            Console.WriteLine("Trafik lambasi hangi renk yaniyor: ");
+            Console.WriteLine("Trafik lambasi hangi renk yaniyor (red, yellow, green): ");
             string renk = Console.ReadLine();
+
+            switch (renk)
+            {
+                case "red": Console.WriteLine("DUR");
+                    break;
+
+                case "yellow":
+                    Console.WriteLine("HAZIRLAN");
+                    break;
+
+                case "green":
+                    Console.WriteLine("GEC");
+                    break;
+
+                default:
+                    break;
+
+            }
+
+
+
+
 
 
 
